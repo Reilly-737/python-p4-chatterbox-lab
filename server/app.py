e39b71644813b7777b96ad994c5386defabaa082
Reilly <reillyswork@gmail.com>
2023-11-20 11:21:39 -0700

    finished lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,50 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        all_messages = [message.to_dict() for message in Message.query.order_by("created_at").all()]
+        return all_messages, 200
+    else:
+        try:
+            req_data = request.get_json()
+            message = Message(**req_data)
+            db.session.add(message) 
+            db.session.commit()
+            return message.to_dict(), 201
+        except:
+            db.session.rollback()
+            return {"error": "Something went wrong!"}, 422
+    
+    
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    if request.method == "PATCH":
+        try:
+            req_data = request.get_json()
+            if message := db.session.get(Message, id):
+                for k, v in req_data.items():
+                    setattr(messages, k, v)
+                db.session.commit()
+                return message.to_dict(), 200
+            return {"error": f"No Message by id {id} found"}, 404
+        except:
+            db.session.rollback()
+            return {"error": "Something went wrong!"}, 422
+    else:
+        try:
+            if message := db.session.get(Message, id):
+                db.session.delete(message)
+                db.session.commit()
+                return {}, 204
+            return {"error": f"No Message by id {id} found"}, 404
+        except:
+            db.session.rollback()
+            return {"error": f"Could not delete Message with id {id}"} 
+            
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -12,3 +12,14 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.String, server_default=db.func.now())
+    updated_at = db.Column(db.String, onupdate=db.func.now())
+
+def __repr(self):
+    return f"""
+        <Message #{self.id}:
+             body: {self.body}
+             username:{self.username}
+    """
